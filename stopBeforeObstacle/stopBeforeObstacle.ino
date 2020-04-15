c09ad37012941b0c62b22ed9eeb9c1ab1a81dec9
Ali <alika@student.chalmers.se>
2020-04-15 13:02:36 +0200

    Adding switch case for manual mode

--- stopBeforeObstacle/stopBeforeObstacle.ino
+++ stopBeforeObstacle/stopBeforeObstacle.ino
@@ -49,3 +49,34 @@ void checkObstacle(){
 
   }
 }
+
+void handleInput() { //handle serial input if there is any
+  if (bluetooth.available()) {
+    if (front_sensor.getDistance()>30){
+    char input;
+    while (bluetooth.available()) { input = bluetooth.read(); }; //read till last character
+    switch (input) {
+      case 'l': //rotate counter-clockwise going forward
+        car.setSpeed(80); //80% of the full speed
+        car.setAngle(-75); //75 degrees to the left
+        break;
+      case 'r': //turn clock-wise
+        car.setSpeed(80);
+        car.setAngle(75);  //75 to the right
+        break;
+      case 'f': //go ahead
+        car.setSpeed(100);
+        car.setAngle(0);
+        break;
+      case 'b': //go back
+        car.setSpeed(-100);
+        car.setAngle(0);
+        break;
+      default: //if you receive something that you don't know, just stop
+        car.setSpeed(0);
+        car.setAngle(0);
+    }
+    }else{ car.setSpeed(0);
+    car.setAngle(0);}
+  }
+}
