d2cb1b04e4ca56cf26f3aee5d525941ef6a1e5d0
Ali <alika@student.chalmers.se>
2020-04-17 01:52:38 +0200

    Adding the Gyroscope

--- stopBeforeObstacle/stopBeforeObstacle.ino
+++ stopBeforeObstacle/stopBeforeObstacle.ino
@@ -9,6 +9,7 @@ BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
 
+GY50 gyroscope(37);
 const auto pulsesPerMeter = 600;
 
 DirectionlessOdometer leftOdometer(
@@ -47,5 +48,36 @@ void checkObstacle(){
 
     car.setSpeed(0);
 
+  }
+}
+
+void handleInput() { //handle serial input if there is any
+  if (bluetooth.available()) {
+    if (front_sensor.getDistance()>30){
+    char input;
+    while (bluetooth.available()) { input = bluetooth.read(); }; //read till last character
+    switch (input) {
+      case 'l': //rotate counter-clockwise going forward
+        car.setSpeed(80); //80% of the full speed
+        car.setAngle(-75); //75 degrees to the left
+        break;
+      case 'r': //turn clock-wise
+        car.setSpeed(80);
+        car.setAngle(75);  //75 to the right
+        break;
+      case 'f': //go ahead
+        car.setSpeed(100);
+        car.setAngle(0);
+        break;
+      case 'b': //go back
+        car.setSpeed(-100);
+        car.setAngle(0);
+        break;
+      default: //if you receive something that you don't know, just stop
+        car.setSpeed(0);
+        car.setAngle(0);
+    }
+    }else{ car.setSpeed(0);
+    car.setAngle(0);}
   }
 }
