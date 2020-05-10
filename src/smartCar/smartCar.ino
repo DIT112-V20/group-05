8c89c6fe335756924f1a93e98189762a9bd3f845
roseke <roseke@student.chalmers.se>
2020-05-10 23:34:24 +0200

    Updated the code layout from 3 files to 1 file Reason for this is to use the webserver to update the car, so we place all the code into the same file.

--- src/smartCar/smartCar.ino
+++ src/smartCar/smartCar.ino
@@ -1,11 +1,27 @@
 #include <Smartcar.h>
 #include <BluetoothSerial.h>
-
-//Include the webserver file
-#include "Webserver.h"
+#include <iostream>
+#include <HTTP_Method.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
 
 //MANUAL (true) OR AUTOMATIC (false) MODE
 boolean nu = true;
+
+//Tell c++ there will be these functions later.
+String sendHTML();
+void handle_OnConnect();
+void handle_NotFound();
+void forwardEndpoint();
+void sensorEndpoint();
+
+//VARIABLES FOR WEBSERVER
+const char* ssid = "";
+const char* password = "";
+WebServer server(12345);
+WiFiClient client;
+String header;
 
 //PINS
 const int FRONT_TRIGGER_PIN = 4; 
@@ -166,4 +182,78 @@ void checkObstacle(){
     if(distance > 0 && distance < 25){
       car.setSpeed(0);
   }
+}
+
+//INITIALIZE THE WEBSERVER
+void webserverInit() {
+
+  //Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+
+  //Print . until WIFI is connected.
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.println(".");
+    delay(1000);
+  }
+
+    //mDNS - connect with https://smartcar.local:12345
+  if(!MDNS.begin("smartcar")){
+    Serial.println("Error! mDNS has not been set up.");
+  }else{
+    Serial.println("mDNS has been setup");
+  }
+
+  //HTTP requests handling
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  server.on("/forward", forwardEndpoint);
+  server.on("/sensor", sensorEndpoint);
+
+  //Print local IP address to the serial monitor and start the web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  Serial.print("Server started!");
+
+  //Add the service to the MDNS-SD
+  MDNS.addService("http", "tcp", 12345);
+}
+
+//CREATE AND UPDATE THE WEBSERVER
+void webserverCreation() {
+
+  server.handleClient();
+  
+}
+
+void handle_OnConnect() {
+  server.send(200, "text/html", sendHTML());
+  Serial.println("Client connected");
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "NOT FOUND");
+}
+
+void forwardEndpoint(){
+}
+
+void sensorEndpoint(){
+  server.send(200, "text/plain", String(front_sensor.getDistance()));
+}
+
+String sendHTML() {
+  String html = "<!DOCTYPE html><html>\n";
+  html += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+  html += "<link rel=\"icon\" href=\"data:,\">\n";
+  html += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  html += ".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;\n";
+  html += "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}\n";
+  html += ".button2 {background-color: #555555;}</style></head>\n";
+  html += "<body><h1>ESP32 Web Server</h1>\n";
+  html += "</body></html>\n";
+
+  return html;
 }
