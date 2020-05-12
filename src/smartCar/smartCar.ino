c5ff0f503530a285090d2581dd5d9ee28458ca14
roseke <roseke@student.chalmers.se>
2020-05-12 22:24:27 +0200

    Fixed naming mistakes

--- src/smartCar/smartCar.ino
+++ src/smartCar/smartCar.ino
@@ -19,7 +19,7 @@ void increaseSpeed();
 void decreaseSpeed();
 void turnOnAutomation();
 void turnOffAutomation();
-void setCarSpeed();
+void setGear();
 void sensorEndpoint();
 
 //VARIABLES FOR WEBSERVER
@@ -169,11 +169,11 @@ void webserverInit() {
   server.on("/turnRight", turnRightEndpoint);
   server.on("/autoOff", turnOffAutomation);
   server.on("/AutoOn", turnOnAutomation);
-  server.on("/setGear", setCarSpeed);
+  server.on("/setGear", setGear);
   server.on("/sensor", sensorEndpoint);
-  server.on("/stop", sensorEndpoint);
-  server.on("/increase", sensorEndpoint);
-  server.on("/decrease", sensorEndpoint);
+  server.on("/stop", stopCar);
+  server.on("/increase", increaseSpeed);
+  server.on("/decrease", decreaseSpeed);
 
   //Print local IP address to the serial monitor and start the web server
   Serial.println("");
@@ -237,8 +237,9 @@ void decreaseSpeed(){
   car.setSpeed(CURRENT_SPEED);
 }
 
-void setCarSpeed(){
-  auto gear = server.argName(1);
+void setGear(){
+  int gear = server.arg(1).toInt();
+  Serial.println(gear);
 
   if (gear = 1){
     CURRENT_SPEED = LOW_SPEED;
@@ -247,6 +248,8 @@ void setCarSpeed(){
   }else if(gear = 3){
     CURRENT_SPEED = HIGH_SPEED;
   }
+
+  server.send(200, "text/html", sendHTML('g'));
 }
 
 void turnOnAutomation(){
@@ -283,6 +286,9 @@ String sendHTML(char message) {
       break;
     case 'l':
       html += "<h2>Turning left!</h2>\n";
+      break;
+    case 'g':
+      html += "<h2>Gear changed!</h2>\n";
       break;
   }
   
