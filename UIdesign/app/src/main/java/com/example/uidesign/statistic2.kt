15789d88657760125f12df6ce563b783b13672ae
mehrvarz <mehrvarz@student.chalmers.se>
2020-05-14 15:07:24 +0200

    gradle file

---
+++ UIdesign/app/src/main/java/com/example/uidesign/statistic2.kt
@@ -0,0 +1,12 @@
+package com.example.uidesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class statistic2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistic2)
+    }
+}
