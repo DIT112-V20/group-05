d5dae09a7ca466bec8f9e182ba1b61b787d4d071
Ali <alika@student.chalmers.se>
2020-05-15 12:23:52 +0200

    activity_automatic class was updated

--- UIdesign/app/src/main/java/com/example/uidesign/activity_automatic.kt
+++ UIdesign/app/src/main/java/com/example/uidesign/activity_automatic.kt
@@ -1,25 +1,13 @@
-package com.example.uieric
+package com.example.alone
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.*
-import androidx.appcompat.app.AlertDialog
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.uieric.MainActivity
-
-
-import kotlinx.android.synthetic.main.activity_automatic.*
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
 class activity_automatic : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +21,7 @@ class activity_automatic : AppCompatActivity() {
 
 
         Statistics.setOnClickListener {
-            val intentback = Intent(this, statstat::class.java)
+            val intentback = Intent(this, Stats::class.java)
             startActivity(intentback)
         }
 
@@ -41,35 +29,17 @@ class activity_automatic : AppCompatActivity() {
 
         // go to manual
 
-        val drive_manual = findViewById(R.id.toManual) as ImageButton
+        val back = findViewById(R.id.toManual) as ImageButton
 
-        drive_manual.setOnClickListener {
-            val manualdrive = Intent(this, ManualActivity2::class.java)
-            startActivity(manualdrive)
+        back.setOnClickListener {
+            val back = Intent(this, MenuActivity::class.java)
+            startActivity(back)
         }
 
     }
 
-    fun sendRequest(endpoint : String){
-        val queue: RequestQueue = Volley.newRequestQueue(applicationContext)
-        val url = "http://213.80.116.220:12345/$endpoint"
-        val stringRequest =
-            StringRequest(Request.Method.GET, url, object : Response.Listener<String?> {
-                override fun onResponse(response: String?) {
-                    Toast.makeText(
-                        applicationContext,
-                        "Command was successful !",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }, object : Response.ErrorListener {
-                override fun onErrorResponse(error: VolleyError?) {
-                    Toast.makeText(applicationContext, "Error occurred", Toast.LENGTH_LONG)
-                        .show()
-                }
-            })
-        queue.add(stringRequest)
-    }
+
 
 
     }
+
