0ec752c27387c6c764ce6d5c0e7ffd820332489e
Ali <alika@student.chalmers.se>
2020-05-18 23:34:58 +0200

    Added a class as a barrier between UI and Volley Request containing the data received from the car

---
+++ UIdesign/app/src/main/java/com/example/uidesign/Data.kt
@@ -0,0 +1,20 @@
+package com.example.uidesign
+
+import android.content.Context
+import android.preference.PreferenceManager
+
+class Data constructor(val context : Context) {
+
+    val request : RequestHandler = RequestHandler(context)
+
+    fun getResponse() : String? {
+        val m = PreferenceManager.getDefaultSharedPreferences(context)
+        var mResponse = m.getString("Response", "")
+        return mResponse
+    }
+
+    fun getDistance() : String? {
+        request.getRequest("sensor")
+        return getResponse()
+    }
+}
