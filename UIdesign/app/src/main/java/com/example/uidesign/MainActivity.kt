fa3d36e17beadaff24d699f51b4801542915eae1
Ali <alika@student.chalmers.se>
2020-05-24 18:57:00 +0200

    added an import that was missed

--- UIdesign/app/src/main/java/com/example/uidesign/MainActivity.kt
+++ UIdesign/app/src/main/java/com/example/uidesign/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 
 
